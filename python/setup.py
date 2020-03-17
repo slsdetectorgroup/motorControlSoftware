54bd33d4e3453c0a5c70681d0157f1a26a0b01c3
Erik Frojdh <erik.frojdh@gmail.com>
2020-03-17 14:16:23 +0100

    python version

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ import sys
 import setuptools
 import os
 
-__version__ = os.environ.get('GIT_DESCRIBE_TAG', 'master')
+__version__ = os.environ.get('GIT_DESCRIBE_TAG', 'developer')
 
 setup(
     name='slsdetbox',
